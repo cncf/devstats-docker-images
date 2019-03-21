33d0237a012ed84809c748134a9d3456bcc1d9dd
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-21 14:07:26 +0000

    Docker images almost working
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- k8s/build_images.sh
+++ k8s/build_images.sh
@@ -10,13 +10,12 @@ cd ../devstats || exit 2
 cd ../devstatscode || exit 3
 
 make replacer sqlitedb || exit 4
-rm -f ../devstats-docker-images/devstatscode.tar 2>/dev/null
-tar cf ../devstats-docker-images/devstatscode.tar cmd vendor *.go replacer sqlitedb
+rm -f ../devstats-docker-images/devstatscode.tar ../devstats-docker-images/grafana-bins.tar 2>/dev/null
+tar cf ../devstats-docker-images/devstatscode.tar cmd vendor *.go
+tar cf ../devstats-docker-images/grafana-bins.tar replacer sqlitedb
 
 cd ../devstats || exit 5
-rm -f ../devstats-docker-images/devstats.tar i../devstats-docker-images/devstats-grafana.tar 2>/dev/null
-#tar cf ../devstats-docker-images/devstats.tar git metrics devel util_sql all cdf lfn shared iovisor mininet opennetworkinglab opensecuritycontroller openswitch p4lang openbmp tungstenfabric cord scripts partials docs cron util_sh/make_binary_links.sh projects.yaml companies.yaml skip_dates.yaml linux.yaml zephyr.yaml github_users.json
-#tar cf ../devstats-docker-images/devstats.tar git metrics cdf devel util_sql envoy all lfn shared iovisor mininet opennetworkinglab opensecuritycontroller openswitch p4lang openbmp tungstenfabric cord scripts partials docs cron zephyr linux kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni jaeger notary tuf rook vitess nats opa spiffe spire cloudevents telepresence helm openmetrics harbor etcd tikv cortex buildpacks falco dragonfly virtualkubelet kubeedge brigade crio cncf opencontainers istio spinnaker knative jsons/.keep util_sh/make_binary_links.sh projects.yaml companies.yaml skip_dates.yaml linux.yaml zephyr.yaml github_users.json
+rm -f ../devstats-docker-images/devstats.tar ../devstats-docker-images/devstats-grafana.tar 2>/dev/null
 tar cf ../devstats-docker-images/devstats.tar git metrics cdf devel util_sql envoy all lfn shared iovisor mininet opennetworkinglab opensecuritycontroller openswitch p4lang openbmp tungstenfabric cord scripts partials docs cron zephyr linux kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni jaeger notary tuf rook vitess nats opa spiffe spire cloudevents telepresence helm openmetrics harbor etcd tikv cortex buildpacks falco dragonfly virtualkubelet kubeedge brigade crio cncf opencontainers istio spinnaker knative jsons/.keep util_sh/make_binary_links.sh projects.yaml companies.yaml skip_dates.yaml github_users.json
 tar cf ../devstats-docker-images/devstats-grafana.tar grafana/shared grafana/img/*.svg grafana/img/*.png grafana/*/change_title_and_icons.sh grafana/*/custom_sqlite.sql grafana/dashboards/*/*.json
 
@@ -26,20 +25,20 @@ tar cf devstats-docker-images.tar k8s
 
 if [ -z "$SKIP_FULL" ]
 then
-  docker build -f ./images/Dockerfile.full -t "${DOCKER_USER}/devstats" .
+  docker build -f ./k8s/Dockerfile.full -t "${DOCKER_USER}/devstats" . || exit 7
 fi
 
 if [ -z "$SKIP_MIN" ]
 then
-  docker build -f ./images/Dockerfile.minimal -t "${DOCKER_USER}/devstats-minimal" .
+  docker build -f ./k8s/Dockerfile.minimal -t "${DOCKER_USER}/devstats-minimal" . || exit 8
 fi
 
 if [ -z "$SKIP_GRAFANA" ]
 then
-  docker build -f ./images/Dockerfile.grafana -t "${DOCKER_USER}/devstats-grafana" .
+  docker build -f ./k8s/Dockerfile.grafana -t "${DOCKER_USER}/devstats-grafana" . || exit 9
 fi
 
-rm -f devstats.tar devstatscode.tar devstats-grafana.tar
+rm -f devstats.tar devstatscode.tar devstats-grafana.tar devstats-docker-images.tar grafana-bins.tar
 exit 1
 
 if [ -z "$SKIP_FULL" ]
