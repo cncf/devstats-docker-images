824c45f55c68cb31dc9f2ec792663cc8ad295892
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-26 13:31:54 +0000

    Add more patching options
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- patches/patch.sh
+++ patches/patch.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-files=`find ./metrics/ -name vars.yaml` || exit 1
+if [ -z "$1" ]
+then
+  echo "$0: you need to specify host type as a first arg: prodsrv or testsrv"
+  exit 1
+fi
+if [ -z "$2" ]
+then
+  echo "$0: you need to specify hostname: devstats-demo.net"
+  exit 2
+fi
+if [ "$1" = "prodsrv" ]
+then
+  fromh='devstats.cncf.io'
+elif [ "$1" = "testsrv" ]
+then
+  fromh='teststats.cncf.io'
+else
+  echo "$0: 1st arg must be either prodsrv or testsrv, got: $1"
+  exit 3
+fi
+files=`find ./metrics/ -name vars.yaml` || exit 4
 for fn in $files
 do
-  echo $fn
-  MODE=ss0 FROM="hostname, os_hostname" TO="hostname, ':devstats-demo.net'" replacer "$fn"
-  MODE=ss0 FROM="':prodsrv=devstats.cncf.io '" TO="':prodsrv=devstats-demo.net '" replacer "$fn"
-  MODE=ss0 FROM="': devstats.cncf.io=prodsrv'" TO="': devstats-demo.net=prodsrv'" replacer "$fn"
+  echo "Patching $fn"
+  MODE=ss0 FROM="hostname, os_hostname" TO="hostname, ':$2'" replacer "$fn"
+  MODE=ss0 FROM="':$1=$fromh '" TO="':$1=$2 '" replacer "$fn"
+  MODE=ss0 FROM="': $fromh=$1'" TO="': $2=$1'" replacer "$fn"
 done
 echo 'Patching OK'
