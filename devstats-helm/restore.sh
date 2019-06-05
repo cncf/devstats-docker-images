0d03274dd5bab6c00a0dba78027ba3cd36811f07
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-05 12:54:49 +0000

    Update patching [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devstats-helm/restore.sh
+++ devstats-helm/restore.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 wget "${RESTORE_FROM}/${PROJDB}.dump" || exit 1
-db.sh psql postgres -c "create database $PROJDB" || exit 2
-db.sh psql postgres -c "grant all privileges on database \"$PROJDB\" to gha_admin" || exit 3
-db.sh psql "$PROJDB" -c "create extension if not exists pgcrypto" || exit 4
-db.sh pg_restore -d "$PROJDB" "$PROJDB.dump" || exit 5
-rm -f "${PROJDB}" || exit 6
+./devel/drop_psql_db.sh "$PROJDB" || exit 2
+db.sh psql postgres -c "create database $PROJDB" || exit 3
+db.sh psql postgres -c "grant all privileges on database \"$PROJDB\" to gha_admin" || exit 4
+db.sh psql "$PROJDB" -c "create extension if not exists pgcrypto" || exit 5
+db.sh pg_restore -d "$PROJDB" "$PROJDB.dump" || echo "returned exist code, but attempt to continue"
+rm -f "${PROJDB}.dump" || exit 6
 db.sh psql "$PROJDB" -c "delete from gha_vars" || exit 7
 GHA2DB_PROJECT="$PROJ" PG_DB="$PROJDB" GHA2DB_LOCAL=1 vars || exit 8
 GHA2DB_PROJECT="$PROJ" PG_DB="$PROJDB" GHA2DB_LOCAL=1 GHA2DB_VARS_FN_YAML="sync_vars.yaml" vars || exit 9
