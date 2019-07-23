4a0098a7ae5dabd8352b87cbd7dc63e856c6d7df
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-23 05:43:03 +0000

    Added annotation.sh script and explained how to use it
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devstats-helm/health.sh
+++ devstats-helm/health.sh
@@ -11,12 +11,12 @@ export GHA2DB_SKIP_METRICS=''
 export GHA2DB_EXCLUDE_VARS=''
 
 ONLY='' ./devel/add_all_annotations.sh || exit 1
-# shoudl be run within devstats-provision-graphql pod, after other project added their annotations
+# shoudl be run within devstats-provision-graphql pod, after other project added their annotations or run devstats-helm/annotations.sh first.
 GHA2DB_PROJECT=graphql PG_DB=graphql GHA2DB_LOCAL=1 GHA2DB_GHAPISKIP=1 GHA2DB_GETREPOSSKIP=1 GHA2DB_SKIPPDB=1 GHA2DB_RESETTSDB=1 GHA2DB_METRICS_YAML=metrics/all/health.yaml GHA2DB_TAGS_YAML=metrics/shared/empty.yaml GHA2DB_COLUMNS_YAML=metrics/shared/empty.yaml gha2db_sync || exit 2
 GHA2DB_PROJECT=graphql PG_DB=graphql GHA2DB_LOCAL=1 GHA2DB_VARS_FN_YAML="sync_vars.yaml" vars || exit 3
-# shoudl be run within devstats-provision-allcdf pod, after other project added their annotations
+# shoudl be run within devstats-provision-allcdf pod, after other project added their annotations or run devstats-helm/annotations.sh first.
 GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_GHAPISKIP=1 GHA2DB_GETREPOSSKIP=1 GHA2DB_SKIPPDB=1 GHA2DB_RESETTSDB=1 GHA2DB_METRICS_YAML=metrics/all/health.yaml GHA2DB_TAGS_YAML=metrics/shared/empty.yaml GHA2DB_COLUMNS_YAML=metrics/shared/empty.yaml gha2db_sync || exit 4
 GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_VARS_FN_YAML="sync_vars.yaml" vars || exit 5
-# shoudl be run within devstats-provision-all pod, after other project added their annotations
+# shoudl be run within devstats-provision-all pod, after other project added their annotations or run devstats-helm/annotations.sh first.
 GHA2DB_PROJECT=all PG_DB=allprj GHA2DB_LOCAL=1 GHA2DB_GHAPISKIP=1 GHA2DB_GETREPOSSKIP=1 GHA2DB_SKIPPDB=1 GHA2DB_RESETTSDB=1 GHA2DB_METRICS_YAML=metrics/all/health.yaml GHA2DB_TAGS_YAML=metrics/shared/empty.yaml GHA2DB_COLUMNS_YAML=metrics/shared/empty.yaml gha2db_sync || exit 6
 GHA2DB_PROJECT=all PG_DB=allprj GHA2DB_LOCAL=1 GHA2DB_VARS_FN_YAML="sync_vars.yaml" vars || exit 7
