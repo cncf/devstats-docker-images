ad8325e4442c69f51196d471aff107dd0f4bbe88
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-18 05:42:31 +0000

    Added grant all script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devstats-helm/grants.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if ( [ -z "$PG_PASS" ] || [ -z "$PG_HOST" ] || [ -z "$PG_PORT" ] )
+then
+  echo "$0: you need to set PG_PASS, PG_HOST and PG_PORT to run this script"
+  exit 1
+fi
+
+export GHA2DB_PROJECTS_YAML="devstats-helm/projects.yaml"
+export LIST_FN_PREFIX="devstats-helm/all_"
+
+. ./devel/all_dbs.sh || exit 2
+for db in $all
+do
+  ./devel/ro_user_grants.sh "$db" || exit 3
+  ./devel/psql_user_grants.sh devstats_team "$db" || exit 4
+done
+
+echo 'OK'
+
