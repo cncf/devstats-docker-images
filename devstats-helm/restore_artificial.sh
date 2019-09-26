ba2d878fdfbad1f7a685ed0ae77e1eb4b684cfc4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-26 14:09:54 +0000

    Update restore script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devstats-helm/restore_artificial.sh
+++ devstats-helm/restore_artificial.sh
@@ -13,23 +13,27 @@ if [ "$db" = "devstats" ]
 then
   exit 0
 fi
-cd /root || exit 1
+cd /root || exit 2
 function finish {
   cd /root
   rm $db.*.tsv
 }
 trap finish EXIT
-xz -k -d $db.tar.xz || exit 2
-tar xf $db.tar || exit 3
-rm $db.tar || exit 4
-db.sh psql $db -tAc "\copy gha_events from '/root/$db.events.tsv'" || exit 5
-db.sh psql $db -tAc "\copy gha_payloads from '/root/$db.payloads.tsv'" || exit 6
-db.sh psql $db -tAc "\copy gha_issues from '/root/$db.issues.tsv'" || exit 7
-db.sh psql $db -tAc "\copy gha_pull_requests from '/root/$db.prs.tsv'" || exit 8
-db.sh psql $db -tAc "\copy gha_milestones from '/root/$db.milestones.tsv'" || exit 9
-db.sh psql $db -tAc "\copy gha_issues_labels from '/root/$db.labels.tsv'" || exit 10
-db.sh psql $db -tAc "\copy gha_issues_assignees from '/root/$db.issue_assignees.tsv'" || exit 11
-db.sh psql $db -tAc "\copy gha_pull_requests_assignees from '/root/$db.pr_assignees.tsv'" || exit 12
-db.sh psql $db -tAc "\copy gha_pull_requests_requested_reviewers from '/root/$db.pr_reviewers.tsv'" || exit 13
-db.sh psql $db -tAc "\copy gha_issues_events_labels from '/root/$db.issues_events_labels.tsv'" || exit 14
-db.sh psql $db -tAc "\copy gha_texts from '/root/$db.texts.tsv'" || exit 15
+if [ ! -z "$RESTORE_FROM" ]
+then
+  wget "${RESTORE_FROM}/backups/${db}.tar.xz" || exit 3
+fi
+xz -k -d $db.tar.xz || exit 4
+tar xf $db.tar || exit 5
+rm $db.tar || exit 6
+db.sh psql $db -tAc "\copy gha_events from '/root/$db.events.tsv'" || exit 7
+db.sh psql $db -tAc "\copy gha_payloads from '/root/$db.payloads.tsv'" || exit 8
+db.sh psql $db -tAc "\copy gha_issues from '/root/$db.issues.tsv'" || exit 9
+db.sh psql $db -tAc "\copy gha_pull_requests from '/root/$db.prs.tsv'" || exit 10
+db.sh psql $db -tAc "\copy gha_milestones from '/root/$db.milestones.tsv'" || exit 11
+db.sh psql $db -tAc "\copy gha_issues_labels from '/root/$db.labels.tsv'" || exit 12
+db.sh psql $db -tAc "\copy gha_issues_assignees from '/root/$db.issue_assignees.tsv'" || exit 13
+db.sh psql $db -tAc "\copy gha_pull_requests_assignees from '/root/$db.pr_assignees.tsv'" || exit 14
+db.sh psql $db -tAc "\copy gha_pull_requests_requested_reviewers from '/root/$db.pr_reviewers.tsv'" || exit 15
+db.sh psql $db -tAc "\copy gha_issues_events_labels from '/root/$db.issues_events_labels.tsv'" || exit 16
+db.sh psql $db -tAc "\copy gha_texts from '/root/$db.texts.tsv'" || exit 17
