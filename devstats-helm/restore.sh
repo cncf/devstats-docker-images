11020e1e9122bea2e50fe72f4769cd03c4ea5a2e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-19 12:24:02 +0000

    Remove wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devstats-helm/restore.sh
+++ devstats-helm/restore.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#wget "${RESTORE_FROM}/${PROJDB}.dump" || exit 1
-#./devel/drop_psql_db.sh "$PROJDB" || exit 2
-#db.sh psql postgres -c "create database $PROJDB" || exit 3
-#db.sh psql postgres -c "grant all privileges on database \"$PROJDB\" to gha_admin" || exit 4
-#db.sh psql "$PROJDB" -c "create extension if not exists pgcrypto" || exit 5
-#db.sh pg_restore -d "$PROJDB" "$PROJDB.dump" || echo "returned exit code, but attempt to continue"
-#rm -f "${PROJDB}.dump" || exit 6
-#db.sh psql "$PROJDB" -c "delete from gha_vars" || exit 7
-#GHA2DB_PROJECT="$PROJ" PG_DB="$PROJDB" GHA2DB_LOCAL=1 vars || exit 8
-#GHA2DB_PROJECT="$PROJ" PG_DB="$PROJDB" GHA2DB_LOCAL=1 GHA2DB_VARS_FN_YAML="sync_vars.yaml" vars || exit 9
-#GHA2DB_PROJECT=$PROJ PG_DB=$PROJDB ./shared/get_repos.sh || exit 10
+wget "${RESTORE_FROM}/${PROJDB}.dump" || exit 1
+./devel/drop_psql_db.sh "$PROJDB" || exit 2
+db.sh psql postgres -c "create database $PROJDB" || exit 3
+db.sh psql postgres -c "grant all privileges on database \"$PROJDB\" to gha_admin" || exit 4
+db.sh psql "$PROJDB" -c "create extension if not exists pgcrypto" || exit 5
+db.sh pg_restore -d "$PROJDB" "$PROJDB.dump" || echo "returned exit code, but attempt to continue"
+rm -f "${PROJDB}.dump" || exit 6
+db.sh psql "$PROJDB" -c "delete from gha_vars" || exit 7
+GHA2DB_PROJECT="$PROJ" PG_DB="$PROJDB" GHA2DB_LOCAL=1 vars || exit 8
+GHA2DB_PROJECT="$PROJ" PG_DB="$PROJDB" GHA2DB_LOCAL=1 GHA2DB_VARS_FN_YAML="sync_vars.yaml" vars || exit 9
+GHA2DB_PROJECT=$PROJ PG_DB=$PROJDB ./shared/get_repos.sh || exit 10
 GHA2DB_PROJECT=$PROJ PG_DB=$PROJDB ./shared/setup_repo_groups.sh || exit 11
 GHA2DB_PROJECT=$PROJ PG_DB=$PROJDB GHA2DB_LOCAL=1 tags || exit 12
 GHA2DB_PROJECT=$PROJ PG_DB=$PROJDB GHA2DB_LOCAL=1 annotations || exit 13
