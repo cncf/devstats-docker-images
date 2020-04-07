9186061b65aa45a6244ef4977da1c119dc098d2f
John DA <lukaszgryglicki@o2.pl>
2020-04-07 06:30:46 +0000

    Update reports config
    Signed-off-by: John DA <lukaszgryglicki@o2.pl>

--- images/build_images.sh
+++ images/build_images.sh
@@ -22,7 +22,7 @@ tar cf ../devstats-docker-images/api-bins.tar api || exit 44
 
 cd ../devstats-reports || exit 40
 rm -f ../devstats-docker-images/devstats-reports.tar 2>/dev/null
-tar cf ../devstats-docker-images/devstats-reports.tar sh sql affs contributors velocity || exit 41
+tar cf ../devstats-docker-images/devstats-reports.tar sh sql affs rep contributors velocity || exit 41
 
 cd ../devstats || exit 7
 rm -f ../devstats-docker-images/index_*.html ../devstats-docker-images/devstats.tar ../devstats-docker-images/devstats-grafana.tar ../devstats-docker-images/*.svg 2>/dev/null
