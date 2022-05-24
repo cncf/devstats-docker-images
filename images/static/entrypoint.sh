398d3188998b543c29245e9757f429d5c68abdff
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-05-24 08:46:04 +0000

    Update paths and more shhink size
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- images/static/entrypoint.sh
+++ images/static/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ls /grafana && rm -rf /usr/share/nginx/html/backups/grafana && mv /grafana /usr/share/nginx/html/backups/grafana
+mv replacer sqlitedb runq /usr/share/nginx/html/backups/grafana
 nginx -g 'daemon off;'
