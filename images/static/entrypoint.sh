dbd3a9beb8c4433deebc78886cb9d8083ae772bc
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-07-15 04:19:09 +0000

    Update static pages images to hanlde shared grafana data
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- images/static/entrypoint.sh
+++ images/static/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-ls /grafana && rm -rf /usr/share/nginx/html/backups/grafana && mv /grafana /usr/share/nginx/html/backups/grafana
-mv replacer sqlitedb runq /usr/share/nginx/html/backups/grafana
+if [ ! -z "${REINIT_SHARED_GRAFANA}" ]
+then
+  ls /grafana && rm -rf /usr/share/nginx/html/backups/grafana && mv /grafana /usr/share/nginx/html/backups/grafana
+  mv replacer sqlitedb runq /usr/share/nginx/html/backups/grafana
+fi
 nginx -g 'daemon off;'
