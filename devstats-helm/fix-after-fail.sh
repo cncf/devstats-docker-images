e92696779096d9ab10f123d7cce8f72c188d8bc5
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-12-20 12:17:49 +0100

    Add util script 2
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- devstats-helm/fix-after-fail.sh
+++ devstats-helm/fix-after-fail.sh
@@ -6,6 +6,9 @@ then
 fi
 echo -n "Confirm that you updated $1/psql.sh or CTRL^C: "
 read
+exec > >(tee 1)
+exec 2> >(tee 2)
+echo "You can track progress via 'tail -f ?'"
 WAITBOOT=1 ORGNAME="-" PORT="-" ICON="-" GRAFSUFF="-" GA="-" SKIPGRAFANA=1 PDB=1 TSDB=1 GHA2DB_MGETC=y ./$1/psql.sh && \
   WAITBOOT=1 ORGNAME="-" PORT="-" ICON="-" GRAFSUFF="-" GA="-" SKIPGRAFANA=1 PDB=1 TSDB=1 GHA2DB_MGETC=y ./devel/ro_user_grants.sh "$1" && \
   WAITBOOT=1 ORGNAME="-" PORT="-" ICON="-" GRAFSUFF="-" GA="-" SKIPGRAFANA=1 PDB=1 TSDB=1 GHA2DB_MGETC=y ./devel/psql_user_grants.sh "devstats_team" "$1" && \
