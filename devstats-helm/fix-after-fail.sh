939589a1085d0e2da2f846bb7b3b535dfe3b2d7f
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-12-20 12:03:54 +0100

    Add util script
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

---
+++ devstats-helm/fix-after-fail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "Usage: $0 <project>"
+  exit 1
+fi
+echo -n "Confirm that you updated $1/psql.sh or CTRL^C: "
+read
+WAITBOOT=1 ORGNAME="-" PORT="-" ICON="-" GRAFSUFF="-" GA="-" SKIPGRAFANA=1 PDB=1 TSDB=1 GHA2DB_MGETC=y ./$1/psql.sh && \
+  WAITBOOT=1 ORGNAME="-" PORT="-" ICON="-" GRAFSUFF="-" GA="-" SKIPGRAFANA=1 PDB=1 TSDB=1 GHA2DB_MGETC=y ./devel/ro_user_grants.sh "$1" && \
+  WAITBOOT=1 ORGNAME="-" PORT="-" ICON="-" GRAFSUFF="-" GA="-" SKIPGRAFANA=1 PDB=1 TSDB=1 GHA2DB_MGETC=y ./devel/psql_user_grants.sh "devstats_team" "$1" && \
+  GHA2DB_ALLOW_METRIC_FAIL=1 WAITBOOT=1 ./devstats-helm/deploy_all.sh
