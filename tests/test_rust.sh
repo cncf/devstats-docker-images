68b0b9b64b8babc8f248a5efc654f3d84f253ad2
Łukasz Gryglicki <lgryglicki@cncf.io>
2026-09-13 08:39:12 +0200

    Porting DevStats to rust - wip - 24
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- tests/test_rust.sh
+++ tests/test_rust.sh
@@ -9,9 +9,12 @@ service postgresql start || exit 1
 sudo -u postgres psql -c "create user gha_admin with password 'pwd' createdb" || exit 2
 sudo -u postgres psql -c "create database dbtest with owner gha_admin lc_collate = 'en_US.UTF-8' lc_ctype = 'en_US.UTF-8' encoding = 'UTF8' template = 'template0'" || exit 3
 git clone https://github.com/cncf/devstatscode || exit 10
+# devstats is needed by the Rust port of devstats' metrics_test.go (rust/devstatscode/tests/metrics_yaml.rs):
+# it runs tests.yaml (kubernetes metric SQL) against fixtures exactly like `make test` in devstats does under Go.
+git clone https://github.com/cncf/devstats || exit 11
 cd devstatscode || exit 12
 hsh=$(git log -n 1 --pretty=format:"%H")
 echo "Using DevStats code hash $hsh"
 cd rust || exit 13
-PG_HOST=127.0.0.1 PG_PORT=5432 PG_USER=gha_admin PG_PASS=pwd ./test.sh || exit 14
+DEVSTATS_DIR="$(pwd)/../../devstats" PG_HOST=127.0.0.1 PG_PORT=5432 PG_USER=gha_admin PG_PASS=pwd ./test.sh || exit 14
 echo 'All tests OK'
